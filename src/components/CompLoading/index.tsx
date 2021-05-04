1cee580f09ce52bbd25f4a1789b8cf66bb408948
Peter Arnold <peter@entroprise.com>
2021-05-04 15:38:09 -0400

    not lottie

--- src/components/CompLoading/index.tsx
+++ src/components/CompLoading/index.tsx
@@ -1,22 +1,29 @@
-import { CSSProperties } from "react";
-import { useLottie } from "lottie-react";
-
-import data from "./data2.json";
+import { Flex, Box, CircularProgress } from "@chakra-ui/react";
+// import { CSSProperties } from "react";
+// import { useLottie } from "lottie-react";
+// import data from "./data2.json";
 
 const CompLoading = () => {
-  const options = {
-    animationData: data,
-    loop: true,
-    autoplay: true,
-    rendererSettings: {
-      preserveAspectRatio: "xMinYMin slice",
-    },
-  };
+  // const options = {
+  //   animationData: data,
+  //   loop: true,
+  //   autoplay: true,
+  //   rendererSettings: {
+  //     preserveAspectRatio: "xMinYMin slice",
+  //   },
+  // };
 
-  const style: CSSProperties = {};
+  // const style: CSSProperties = {};
 
-  const { View } = useLottie(options, style);
-  return View;
+  // const { View } = useLottie(options, style);
+  // return View;
+  return (
+    <Flex justifyContent="center">
+      <Flex height="100vh" flexDirection="column" justifyContent="center">
+        <CircularProgress isIndeterminate />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default CompLoading;
