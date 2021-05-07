daccd45714f0ed65953d88449f240a8f92156295
Peter Arnold <peter@entroprise.com>
2021-05-07 18:48:52 -0400

    work

--- src/pages/client/auth/signin.js
+++ src/pages/client/auth/signin.js
@@ -1,12 +1,18 @@
+import { useRouter } from "next/router";
 import { getProviders, signIn } from "next-auth/client";
 import { Button, Box } from "@chakra-ui/react";
 
 export default function SignIn({ providers }) {
+  const router = useRouter();
   return (
     <>
       {Object.values(providers).map((provider) => (
         <Box key={provider.name}>
-          <Button onClick={() => signIn(provider.id)}>
+          <Button
+            onClick={() =>
+              signIn(provider.id, { callbackUrl: router.query.callbackUrl })
+            }
+          >
             Sign in with {provider.name}
           </Button>
         </Box>
