4dbda49b4001719447f61967767ab622b3dac131
Peter Arnold <peter@entroprise.com>
2021-05-10 12:39:46 -0400

    pwa2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -40,19 +40,12 @@ function App({ Component, pageProps }: AppProps) {
 
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="/chrome/chrome-favicon-16-16.png"
           rel="icon"
           type="image/png"
           sizes="16x16"
         />
-        <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
+        <meta name="theme-color" content="#2D29D6" />
       </Head>
       <Provider session={pageProps.session}>
         <ChakraProvider resetCSS theme={theme}>
