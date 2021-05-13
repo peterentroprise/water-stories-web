306956a4b457e77ffb194ab91d49dc80c4235122
Peter Arnold <peter@entroprise.com>
2021-05-13 16:56:24 -0400

    swr

--- src/pages/client/browse.js
+++ src/pages/client/browse.js
@@ -20,23 +20,30 @@ const Browse = (props) => {
 
   const [session, loading] = useSession();
 
-  const variables = {};
+  const tier_id =
+    (session && session.user.content_tier) || "ckom1y4sw0rpn0b70rca9xby8";
+
+  const variables = {
+    id: tier_id,
+  };
+
   const headers = {
-    // authorization: `Bearer ${session.token}`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMDEzNzk0NzU5NjMwNDA1OTUyODUiLCJ1c2VyX2lkIjoiMTAxMzc5NDc1OTYzMDQwNTk1Mjg1IiwibmFtZSI6IlBldGVyIEFybm9sZCIsImVtYWlsIjoicGV0ZXJAZW50cm9wcmlzZS5jb20iLCJpbWFnZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FBVFhBSnlYQUlGNDZWdVh2SGhHV25iYldJS3FvemdCQlNzWVRzVVY3UkxyPXM5Ni1jIiwiaWF0IjoxNjIwOTI3NTUxLjk5OSwiZXhwIjoxNjIxMDEzOTUxLCJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsiYWRtaW4iLCJ1c2VyIl0sIngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS1yb2xlIjoidXNlciIsIngtaGFzdXJhLXVzZXItaWQiOiIxMDEzNzk0NzU5NjMwNDA1OTUyODUifSwiY29udGVudF90aWVyIjoiY2tvbTF5c3EwMHJ3NjBiNzA3ZDVrd2x5MSJ9.RYxsKi96BzEQWULhXJJ7s7nuEpvdxZbpD6w4GYmgog0`,
     // "x-hasura-admin-secret": HASURA_ADMIN_SECRET,
   };
 
-  // const { data: contenTier, error } = useSWR(
-  //   [GET_STORIES_BY_TIER, variables, headers],
-  //   fetcher,
-  //   {
-  //     initialData,
-  //   }
-  // );
+  const { data: contentTier, error } = useSWR(
+    [GET_STORIES_BY_TIER, variables, headers],
+    fetcher,
+    {
+      initialData,
+    }
+  );
 
-  // if (error) return <>failed to load</>;
-  // if (!contenTier) return <>loading...</>;
-  // const content = initialData.contentTier.storyGroups[0].stories;
+  if (error) return <>failed to load</>;
+  if (!contentTier) return <>loading...</>;
+
+  console.log(initialData);
 
   const content = initialData.contentTier.storyGroups.reduce(
     (list, storyGroup) => {
@@ -61,8 +68,8 @@ export async function getServerSideProps(context) {
   };
 
   const headers = {
-    // authorization: `Bearer ${token}`,
-    "x-hasura-admin-secret": HASURA_ADMIN_SECRET,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMDEzNzk0NzU5NjMwNDA1OTUyODUiLCJ1c2VyX2lkIjoiMTAxMzc5NDc1OTYzMDQwNTk1Mjg1IiwibmFtZSI6IlBldGVyIEFybm9sZCIsImVtYWlsIjoicGV0ZXJAZW50cm9wcmlzZS5jb20iLCJpbWFnZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FBVFhBSnlYQUlGNDZWdVh2SGhHV25iYldJS3FvemdCQlNzWVRzVVY3UkxyPXM5Ni1jIiwiaWF0IjoxNjIwOTI3NTUxLjk5OSwiZXhwIjoxNjIxMDEzOTUxLCJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsiYWRtaW4iLCJ1c2VyIl0sIngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS1yb2xlIjoidXNlciIsIngtaGFzdXJhLXVzZXItaWQiOiIxMDEzNzk0NzU5NjMwNDA1OTUyODUifSwiY29udGVudF90aWVyIjoiY2tvbTF5c3EwMHJ3NjBiNzA3ZDVrd2x5MSJ9.RYxsKi96BzEQWULhXJJ7s7nuEpvdxZbpD6w4GYmgog0`,
+    // "x-hasura-admin-secret": HASURA_ADMIN_SECRET,
   };
 
   const data = await fetcher(GET_STORIES_BY_TIER, variables, headers);
