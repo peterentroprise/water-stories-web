d2fcfada3e99f5e87091c57c17eb02106efb958f
Peter Arnold <peter@entroprise.com>
2021-06-07 10:57:45 -0400

    night wortk

--- src/pages/client/explore.tsx
+++ src/pages/client/explore.tsx
@@ -1,12 +1,5 @@
-// import dynamic from "next/dynamic";
-// import CompLoading from "../../components/CompLoading";
 import PageExplore from "components/PageExplore";
 import LayoutApp from "components/LayoutApp";
-
-// const PageExplore = dynamic(() => import("../../components/PageExplore"), {
-//   loading: () => <CompLoading />,
-//   ssr: false,
-// });
 
 const Explore = () => {
   return <PageExplore />;
